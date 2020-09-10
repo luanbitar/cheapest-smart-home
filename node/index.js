64fd1ff8c26950cf5f4147d35065cfd4b31e38a7
luanbitar <7@lbitar.com>
2020-09-10 16:34:15 -0300

    feat: adding etherport fix

--- node/index.js
+++ node/index.js
@@ -2,17 +2,39 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const five = require("johnny-five")
-const board = new five.Board()
+const EtherPortClient = require("etherport-client").EtherPortClient;
+
+const espConfig = new EtherPortClient({
+  host: "192.168.1.102",
+  port: 3030
+})
 
 const app = module.exports = express()
 let led = null
 
 const setLedPower = power => (req, res) => {
   if(!led) return
-  if(power === 'TOGGLE') led.toggle()
+  if(power === 'TOGGLE') {
+    led.off()
+    setTimeout(() => {
+      led.on()
+    }, 500)
+  }
   else if(power) led.on()
   else led.off()
-  if(res) res.send('Hello from root route.' + JSON.stringify(power))
+  if(res) res.send('Hello from root route. led pulsed')
+}
+
+function onConnect(req, res) {
+  const esp = new five.Board({
+    port: espConfig,
+    repl: false
+  })
+
+  esp.on("ready", () => {
+    led = new five.Led(2)
+    res.send("connected to esp")
+  })
 }
 
 app.use(helmet())
@@ -20,8 +42,9 @@ app.use(cors())
 app.get('/', setLedPower('TOGGLE'))
 app.get('/on', setLedPower(true))
 app.get('/off', setLedPower(false))
+app.get('/connect', onConnect)
 
-board.on("ready", () => led = new five.Led(13))
+
 
 if(!module.parent)
   app.listen(3001)
